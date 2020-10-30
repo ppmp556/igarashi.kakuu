a3abedf2afd25e5a35c284ce628ba8f682ddb287
ppmp556 <pino_56_0825@yahoo.co.jp>
2020-10-30 16:34:46 +0900

    test

---
+++ .history/menu_20200617221206.js
@@ -0,0 +1,26 @@
+const body = document.body;
+
+const toggler = document.getElementById('navToggler');
+//#navToggleを取得し、togglerに格納
+
+
+const navList = document.getElementById('js-nav');
+const navList2 = document.getElementById('js-nav2');
+//#navListsを取得し、navListに格納
+
+
+toggler.addEventListener('click', function() {　
+    //togglerクリックされたら、
+    body.classList.toggle('clicked');　
+    
+});
+navList.addEventListener('click', function(){
+
+    
+    body.classList.remove('clicked');
+});
+navList2.addEventListener('click', function(){
+
+    
+    body.classList.remove('clicked');
+});
